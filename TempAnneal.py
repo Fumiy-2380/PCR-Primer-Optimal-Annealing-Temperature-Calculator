a40350c60f2c33341c56971c659bc71d520cb249
Fumiy-2380 <61441690+Fumiy-2380@users.noreply.github.com>
2021-11-01 11:11:51 -0700

    Update TempAnneal.py
    Changed primer sequence input from a .txt file input to a manual sequence input for forward and reverse primers.

--- TempAnneal.py
+++ TempAnneal.py
@@ -10,11 +10,15 @@ root = tk.Tk()
 root.withdraw()
 
 #Import primer sequences and the product sequence
-primer_seq = filedialog.askopenfilename()
+#primer_seq = filedialog.askopenfilename()
+F_primer = input("Input forward primer sequence: ")
+R_primer = input("Input reverse primer sequence: ")
 product_seq = filedialog.askopenfilename() 
 
+"""
 with open(primer_seq, "r") as primer_intro:
     primers = [line.strip("\n") for line in primer_intro]
+"""
 with open(product_seq, "r") as product_intro:
     product = product_intro.read()
 
@@ -28,12 +32,12 @@ l = len(product) #Length of product in bp
 
 #Table - Nearest-neighbor thermodynamics [dH (kcal), dS (cal/K), dG (kcal)]
 AAorTT = [9.1, 24E-3, 1.9] 
-AT = [8.6, 23.9E-3, 1.5] #unique
-ACorGT = [6.5, 17.3E-3, 1.3] #GT
-AGorCT = [7.8, 20.8E-3, 1.6] #CT
-TA = [6.0, 16.9E-3, 0.9] #Unique
-TCorGA = [5.6, 13.5E-3, 1.6] #GA
-TGorCA = [5.8, 12.9E-3, 1.9] #CA
+AT = [8.6, 23.9E-3, 1.5] 
+ACorGT = [6.5, 17.3E-3, 1.3] 
+AGorCT = [7.8, 20.8E-3, 1.6] 
+TA = [6.0, 16.9E-3, 0.9] 
+TCorGA = [5.6, 13.5E-3, 1.6] 
+TGorCA = [5.8, 12.9E-3, 1.9] 
 CCorGG = [11.0, 26.6E-3, 3.1]
 CG = [11.9, 27.8E-3, 3.6]
 GC = [11.1, 26.7E-3, 3.1]
@@ -97,19 +101,24 @@ def GC_Content(product_seq):
 
 #Run calculations for each strand   
 Tm_product = 0.41*(GC_Content(product))+16.6*math.log(K,10)-675/l
-primer_number = 1
-for seq in primers:
-    dH, dS = dH_dS_finder(seq)
+#primer_number = 1
+#or seq in primers:
+def Ta_Calculator(primer):
+    primer_number = 1
+    dH, dS = dH_dS_finder(primer)
     Tm_primer = dH/(dS+R*math.log(c/4))-273.15+16.6*math.log(K,10)
     Opt_Ta = 0.3*Tm_primer+0.7*Tm_product-14.9
-    worksheet.write(primer_number, 1, Opt_Ta)
-    worksheet.write(primer_number, 2, seq)
     primer_number += 1
-
+    return Opt_Ta
+ 
 #Input results into excel sheet
 worksheet.write(1,0,"F. Primer")
 worksheet.write(2,0,"R. Primer")
 worksheet.write(0,1,"Opt Ta")
 worksheet.write(0,2,"Primer Sequence")
+worksheet.write(1,1,Ta_Calculator(F_primer))
+worksheet.write(2,1,Ta_Calculator(R_primer))
+worksheet.write(1,2,F_primer)
+worksheet.write(2,2,R_primer)
 workbook.close()
 print("Analysis complete.")
